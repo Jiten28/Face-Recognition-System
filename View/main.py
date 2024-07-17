dd3fb7c4d091f3f2dbdd59304a93419f0beda607
Jiten Kumar <jiten282003@gmail.com>
2024-07-17 19:02:47 +0530

    Added a Background for Face-Recognition based Attendence system

--- View/main.py
+++ View/main.py
@@ -1,13 +1,16 @@
 import cv2
 
 cap = cv2.VideoCapture(1)
-cap.set(3,640)
-cap.set(4,480)
+cap.set(3, 640)
+cap.set(4, 480)
 
-imgBackground = cv2.imread('Resources\background.png')
+imgBackground = cv2.imread('Resources/background.png')
 
 while True:
     success, img = cap.read()
     
-    cv2.imshow("Face Detection", img)
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    
+    # cv2.imshow("webcam", img)
+    cv2.imshow("Face Detection", imgBackground)
     cv2.waitKey(1)
