abde476eaf4b26a558afad2c6e35f8283d89df28
Jiten Kumar <77165984+Jiten28@users.noreply.github.com>
2024-11-26 21:28:27 +0530

    connected main file with modes images

--- View/main.py
+++ View/main.py
@@ -1,23 +1,27 @@
 import cv2
-
 import os
 
 cap = cv2.VideoCapture(1)
 cap.set(3, 640)
 cap.set(4, 480)
 
-imgBackground = cv2.imread('Resources/background.png')
+imgBackground = cv2.imread('Resources\\background.png')
 
 #Importing the mode images into a list
-folderModePath = 'Resources/Modes'
+folderModePath = "Resources\\Modes"
 modePathList = os.listdir(folderModePath)
-imgModeList = []
+imgModeList = [ ]
+
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath,path)))
+print(len(imgModeList))
 
 while True:
     success, img = cap.read()
     
     imgBackground[162:162 + 480, 55:55 + 640] = img
-    
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[0]
+        
     # cv2.imshow("webcam", img)
     cv2.imshow("Face Detection", imgBackground)
     cv2.waitKey(1)
