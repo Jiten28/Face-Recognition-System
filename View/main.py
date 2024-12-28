aaf6a821f7396c8d1f56e90ce19f92caed0f6d55
Jiten Kumar <77165984+Jiten28@users.noreply.github.com>
2024-12-28 19:29:46 +0530

    Comparing and matching the face

--- View/main.py
+++ View/main.py
@@ -1,6 +1,9 @@
-import cv2
 import os
 import pickle
+import numpy as np
+import cv2
+import face_recognition
+
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -24,14 +27,29 @@ f = open(file, 'rb')
 encodeListKnownWithIds = pickle.load(f)
 f.close()
 encodeListKnown, studentIds = encodeListKnownWithIds
-print(studentIds)
+# print(studentIds)
 print("Encode file Loaded Successfully")
 
 while True:
     success, img = cap.read()
     
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+    
     imgBackground[162:162 + 480, 55:55 + 640] = img
     imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[0]
+    
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        print("Matches", matches)
+        print("Face Distance", faceDis)
+        
+        matchIndex = np.argmin(faceDis)
+        print("Match Index", matchIndex)
         
     # cv2.imshow("webcam", img)
     cv2.imshow("Face Detection", imgBackground)
